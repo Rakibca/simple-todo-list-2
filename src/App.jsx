fc134209aadb52d527866391809e96045478604a
Rakibul Islam <rakib.ca@gmail.com>
2024-04-17 10:31:28 -0600

    Delete Todos added

--- src/App.jsx
+++ src/App.jsx
@@ -36,20 +36,7 @@ function App() {
       <ul id="list">
         {todos.map((todo) => {
           return (
-            <li key={todo.id} className="list-item">
-              <label className="list-item-label">
-                <input
-                  checked={todo.completed}
-                  type="checkbox"
-                  data-list-item-checkbox
-                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
-                />
-                <span data-list-item-text>{todo.name}</span>
-              </label>
-              <button onClick={() => deleteTodo(todo.id)} data-button-delete>
-                Delete
-              </button>
-            </li>
+
           );
         })}
       </ul>
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const TodoItem = () => {
-  return <div>TodoItem</div>;
+  return (
+    <li key={todo.id} className="list-item">
+      <label className="list-item-label">
+        <input
+          checked={todo.completed}
+          type="checkbox"
+          data-list-item-checkbox
+          onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+        />
+        <span data-list-item-text>{todo.name}</span>
+      </label>
+      <button onClick={() => deleteTodo(todo.id)} data-button-delete>
+        Delete
+      </button>
+    </li>
+  );
 };
 
 export default TodoItem;
